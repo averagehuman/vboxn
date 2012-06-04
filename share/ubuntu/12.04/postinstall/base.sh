9889d5e735403f49061455a96970e77024f1be9f
podados <gflanagan@devopsni.com>
2012-06-04 18:45:55 +0100

    ubuntu kickstart files from veewee

---
+++ share/ubuntu/12.04/postinstall/base.sh
@@ -0,0 +1,32 @@
+# postinstall.sh created from Mitchell's official lucid32/64 baseboxes
+
+# Setup sudo to allow no-password sudo for "admin"
+groupadd -r admin
+cp /etc/sudoers /etc/sudoers.orig
+sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
+sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
+
+
+# Apt-install various things necessary for Ruby, guest additions,
+# etc., and remove optional things to trim down the machine.
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install linux-headers-$(uname -r) build-essential
+apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
+apt-get -y install vim
+apt-get clean
+
+# Installing the virtualbox guest additions
+apt-get -y install dkms
+VBOX_VERSION="${VBOX_VERSION:=4.1.16}"
+cd /tmp
+wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
+mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+
+rm VBoxGuestAdditions_$VBOX_VERSION.iso
+
+# Install NFS client
+apt-get -y install nfs-common
+
